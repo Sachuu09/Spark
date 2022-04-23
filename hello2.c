5337183dba05adb153cbd8343bb7868e7e7947c7
Sachuu09 <chavansachhu09@gmail.com>
2022-04-23 10:42:20 +0530

    This Is First From Bash, adding c

---
+++ hello2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+main()
+{
+	printf("Your System Hack");
+}
---
+++ mul.c
@@ -0,0 +1,10 @@
+<include stdio.h>
+
+main()
+{
+	int mul=0,a=2,b=2;
+
+        mul=a*b;
+
+	printf("Multiplication Is = ",mul);
+}w
